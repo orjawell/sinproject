df7627e17cf00f5eb900a77f8346dfa356eb14b0
Akuramu <69430732+Akuramu@users.noreply.github.com>
2020-09-25 10:43:38 +0200

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -1,9 +1,9 @@
 function CalculAge() {  
     var today = new Date();
-    var dtn=document.getElementById('DateNais').value; // on lit la date de naissance
-    var an=dtn.substr(6,4); // l'année (les quatre premiers caractères de la chaîne à partir de 6)
-    var mois=dtn.substr(3,2);// On selectionne le mois de la date de naissance
-    var day= dtn.substr(0,2); // On selectionne la jour de la date de naissance
+    var dtn=document.getElementById('DateNais').value;
+    var an=dtn.substr(6,4); 
+    var mois=dtn.substr(3,2):
+    var day= dtn.substr(0,2);
 
     var dateNaissance = new Date(an + "-" + mois + "-" + day);
 
@@ -13,5 +13,5 @@ function CalculAge() {
         age = age - 1;
     }
   
-    document.getElementById('Age').value = age; // que l'on place dans le input d'id Age
+    document.getElementById('Age').value = age;
 }
